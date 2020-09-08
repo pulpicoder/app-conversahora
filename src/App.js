91321caa39274d7ebeba72e7d2ab89bb7d96ac31
Isaac Baron <isaacbaron.designer@gmail.com>
2020-09-08 09:27:26 -0500

    Creating development enviroment, React, Firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+// Import React
 import React from 'react';
-import logo from './logo.svg';
+
+// Import Components
+import {Switch, Route} from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+
+// Import styles of component App
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+
+        <Route exact path='/'>
+            <h2>Home</h2>
+        </Route>
+        
+      </Switch>
+      <Footer />
     </div>
   );
 }
